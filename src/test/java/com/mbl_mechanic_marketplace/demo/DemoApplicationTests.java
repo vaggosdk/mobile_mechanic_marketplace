2bcbfcefff955016e85bc317f46e75cedb164f23
vagstavrou <vaggosdk@gmail.com>
2026-07-07 22:34:31 +0300

    fix for path in test class

--- src/test/java/com/mbl_mechanic_marketplace/demo/DemoApplicationTests.java
+++ src/test/java/com/mbl_mechanic_marketplace/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package test.java.com.mbl_mechanic_marketplace.demo;
+package com.mbl_mechanic_marketplace.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
