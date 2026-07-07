d6de2952a06e18dd68f25b8d4554d9110280a94a
vagstavrou <vaggosdk@gmail.com>
2026-07-07 22:32:43 +0300

    application name changed 2 and fix for path in class

--- src/main/java/com/mbl_mechanic_marketplace/demo/DemoApplication.java
+++ src/main/java/com/mbl_mechanic_marketplace/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.mbl_mechanic_marketplace.demo;
+package com.mbl_mechanic_marketplace.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
